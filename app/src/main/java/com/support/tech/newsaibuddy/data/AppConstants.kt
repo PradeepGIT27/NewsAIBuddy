0523bf33b3319bac74f0134d7a7ec3f3ac5748fb
Pradeep BG <pradeep.s@bank-genie.com>
2025-04-20 15:18:04 +0530

    News added with category tabs and response. (Official API- Key added)

--- app/src/main/java/com/support/tech/newsaibuddy/data/AppConstants.kt
+++ app/src/main/java/com/support/tech/newsaibuddy/data/AppConstants.kt
@@ -3,6 +3,7 @@ package com.support.tech.newsaibuddy.data
 object AppConstants {
     const val NEWS_URL = "https://newsapi.org/"
     const val COUNTRY = "us"
-    const val API_KEY = "f74c9598d93a4bec879dc68136c870d1"
+//    const val API_KEY = "f74c9598d93a4bec879dc68136c870d1" //Personal_account
+    const val API_KEY = "70c6926ad7bf4b97b697e5e26697278b" //Official_account
     const val CATEGORY = "technology"
 }
