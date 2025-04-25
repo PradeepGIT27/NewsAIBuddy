138ba7dcc3c7c973805e1c214b909a459bbbc405
Pradeep BG <pradeep.s@bank-genie.com>
2025-04-25 07:28:31 +0530

    APi Keys are implied to build confiq file.

--- app/src/main/java/com/support/tech/newsaibuddy/data/api/ApiService.kt
+++ app/src/main/java/com/support/tech/newsaibuddy/data/api/ApiService.kt
@@ -1,6 +1,6 @@
 package com.support.tech.newsaibuddy.data.api
 
-import com.support.tech.newsaibuddy.data.AppConstants
+import com.support.tech.newsaibuddy.BuildConfig
 import com.support.tech.newsaibuddy.data.entity.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -14,7 +14,7 @@ interface ApiService {
         @Query("apiKey") apiKey: String
     ): Response<NewsResponse>
 
-    @Headers("X-Api-Key: ${AppConstants.API_KEY}")
+    @Headers("X-Api-Key: ${BuildConfig.NEWS_API_KEY}")
     @GET("v2/top-headlines")
     suspend fun getTopHeadlinesBasedOnCategory(
         @Query("category") country: String,
--- app/src/main/java/com/support/tech/newsaibuddy/ui/screens/chatbot/ChatBotScreen.kt
+++ app/src/main/java/com/support/tech/newsaibuddy/ui/screens/chatbot/ChatBotScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
